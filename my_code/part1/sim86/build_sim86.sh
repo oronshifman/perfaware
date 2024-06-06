ad085257ab8bd0361abf25c2d950efae3c016df4
oron.shifman <oronshifman@gmail.com>
2024-06-06 09:11:19 +0300

    changed repo structure

--- my_code/part1/sim86/build_sim86.sh
+++ my_code/part1/sim86/build_sim86.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+mkdir -p ../build
+pushd ../build
 
 # with decoder
-gcc -I. sim86_instruction.c sim86_manager.c sim86_printer.c sim86_executer.c sim86_decoder.c sim86_memory.c sim86_main.c -o sim86.out -g
+gcc ../sim86/*.c -o sim86.out -g -I/home/oron/git/perfaware/my_code/part1/sim86
 
 # without decoder
-# gcc -I. sim86_manager.c sim86_printer.c sim86_executer.c sim86_memory.c sim86_main.c -o sim86 -g
+# gcc -I/home/oron/git/perfaware/my_code/part1/sim86 sim86_manager.c sim86_printer.c sim86_executer.c sim86_memory.c sim86_main.c -o sim86 -g
+
+popd

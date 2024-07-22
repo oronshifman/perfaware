75b66b9ec041a5c69f017f07ce921f01d2ee65a6
oron.shifman <oronshifman@gmail.com>
2024-07-22 16:21:27 +0300

    first commit for JSORONParser

---
+++ JSORONParser/include/JSORONParser.h
@@ -0,0 +1,16 @@
+/* ------------------------------------------*/ 
+/* Filename: JSORONParser.h                  */
+/* Date:     22.07.2024                      */
+/* Author:   Oron                            */ 
+/* ------------------------------------------*/
+
+#ifndef __JSORON_PARSER_H__
+#define __JSORON_PARSER_H__
+
+class JSORONParser 
+{
+public:
+
+};
+
+#endif /* JSORON_PARSER_H */
---
+++ JSORONParser/test/JSORONParser_main.cpp
@@ -0,0 +1,14 @@
+/* ------------------------------------------*/ 
+/* Filename: JSORONParser_main.cpp           */
+/* Date:     22.07.2024              	     */
+/* Author:   Oron                            */ 
+/* ------------------------------------------*/
+
+#include "JSORONParser.h"
+
+int main(int argc, char *argv[])
+{
+
+	return 0;
+}
+

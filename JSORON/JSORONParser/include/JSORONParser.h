d30c0aea31cc3a3d6bdbc2d17a995c09d8d4af75
oron.shifman <oronshifman@gmail.com>
2024-07-24 16:42:06 +0300

    created project file tree for JSORONParser

---
+++ JSORON/JSORONParser/include/JSORONParser.h
@@ -0,0 +1,17 @@
+/* ------------------------------------------*/ 
+/* Filename: JSORONParser.h                  */
+/* Date:     24.07.2024                      */
+/* Author:   Oron                            */ 
+/* ------------------------------------------*/
+
+#ifndef __JSORONPARSER_H__
+#define __JSORONPARSER_H__
+
+class JSORONParser 
+{
+public:
+
+};
+
+#endif /* JSORONPARSER_H */
+
